b14e16aded77718f456ea647130b7971d552dc77
H41s3 <calmaemiliojoseph13.8@gmail.com>
2026-05-13 23:46:50 +1000

    chore: Remove unused updateBirthControl from BirthControlManager

--- src/components/cycle/BirthControlManager.tsx
+++ src/components/cycle/BirthControlManager.tsx
@@ -11,7 +11,6 @@ export default function BirthControlManager() {
     activeBirthControl,
     fetchBirthControls,
     addBirthControl,
-    updateBirthControl,
     deactivateBirthControl,
   } = useBirthControlStore();
 
